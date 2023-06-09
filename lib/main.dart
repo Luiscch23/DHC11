2823c550c7633c2e147b28b34c4904995594231f
Luis <l_Carreon@uadec.edu.mx>
2023-06-09 05:11:48 -0600

    segundo cambio

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class _MyAppState extends State<MyApp> {
       return MaterialApp(
         home: Scaffold(
           appBar: AppBar(
-            title: Text('Temperatura'),
+            title: Text('Temperaturaaaaaa'),
           ),
           body: ListView(
             children: [
